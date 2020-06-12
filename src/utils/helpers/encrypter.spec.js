bb90157476e0fbfdf573895a5bdb1b3f25340102
Gregory Casanova <gregorynewhouse@hotmail.com>
2020-06-12 16:40:09 -0300

    ensure Encrypter returns false if bcrypt returns false

---
+++ src/utils/helpers/encrypter.spec.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt')
+
+class Encrypter {
+  async compare (value, hash) {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
+  }
+}
+
+describe('Ecrypter', () => {
+  test('should return true if bcrypt returns true', async () => {
+    const sut = new Encrypter()
+    const isValid = await sut.compare('any_value', 'hashed_value')
+    expect(isValid).toBe(true)
+  })
+
+  test('should return false if bcrypt returns true', async () => {
+    const sut = new Encrypter()
+    bcrypt.isValid = false
+    const isValid = await sut.compare('any_value', 'hashed_value')
+    expect(isValid).toBe(false)
+  })
+})
