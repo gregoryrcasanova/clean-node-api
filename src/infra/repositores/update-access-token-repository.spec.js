55b26bd47cab82d17acb3b76b6511e22f9f87ae0
Gregory Casanova <gregorynewhouse@hotmail.com>
2020-06-14 14:14:34 -0300

    move UpdateAccessTokenRepository to its own file

--- src/infra/repositores/update-access-token-repository.spec.js
+++ src/infra/repositores/update-access-token-repository.spec.js
@@ -1,27 +1,6 @@
 const MongoHelper = require('../helpers/mongo-helper')
 const { MissingParamError } = require('../../utils/errors')
-
-class UpdateAccessTokenRepository {
-  constructor (userModel) {
-    this.userModel = userModel
-  }
-
-  async update (userId, accessToken) {
-    if (!userId) {
-      throw new MissingParamError('userId')
-    }
-    if (!accessToken) {
-      throw new MissingParamError('accessToken')
-    }
-    await this.userModel.updateOne({
-      _id: userId
-    }, {
-      $set: {
-        accessToken
-      }
-    })
-  }
-}
+const UpdateAccessTokenRepository = require('./update-access-token-repository')
 
 const makeSut = (db) => {
   const userModel = db.collection('users')
